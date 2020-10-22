9465c46201298e035440f0a54b7662291e71f217
Zhi Li <lzblack@users.noreply.github.com>
2020-10-22 18:05:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -25,38 +25,12 @@ def calculate():
         a = float(request.form["a"])
         b = float(request.form["b"])
         c = float(request.form["c"])
-        result = quadratic(a, b, c)
+        root_1, root_2 = quadratic(a, b, c)
 
-        if result is not None:
-            if isinstance(result, float):
-                print(f'The solution is {result}.')
-                return render_template(
-                    "calculator_result.html",
-                    a=a,
-                    b=b,
-                    c=c,
-                    root_1=result,
-                    root_2=result,
-                )
-            else:
-                return render_template(
-                    "calculator_result.html",
-                    a=a,
-                    b=b,
-                    c=c,
-                    root_1=result[0],
-                    root_2=result[1],
-                )
-        return render_template("calculator_form.html", error=True)
+        if root_1:
+            return render_template(
+                "calculator_result.html", a=a, b=b, c=c, root_1=root_1, root_2=root_2
+            )
+        else:
+            return render_template("calculator_form.html", error=True)
     return render_template("calculator_form.html", error=None)
-
-
-@app.route("/grade")
-def test():
-    grades = [
-        {'name': 'John', 'grade': 80},
-        {'name': 'Paul', 'grade': 90},
-        {'name': 'George', 'grade': 85},
-        {'name': 'Ringo', 'grade': 95},
-    ]
-    return render_template("grades.html", grades=grades)
